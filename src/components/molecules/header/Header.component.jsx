785900c49fdc13b556f60fd21c29775f65e6b3e1
isma3l <karlo.ismael@gmail.com>
2023-04-19 01:04:22 +0200

    💄 icon is displayed in the header

--- src/components/molecules/header/Header.component.jsx
+++ src/components/molecules/header/Header.component.jsx
@@ -1,10 +1,12 @@
-// import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+import { Logo } from '@/assets'
 
 const HeaderComponent = () => {
   return (
     <div className="border-b-2 border-gray-300 py-3 flex justify-between">
-      Products header
+      <Link className="text-3xl font-semibold text-sky-600" to="/">
+        <img src={Logo} className="h-10" alt="React Logo" />
+      </Link>
     </div>
   )
 }
