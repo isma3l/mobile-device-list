d9a0a0515a5085820cb2cc4dc2a9075f42bcd2a5
isma3l <karlo.ismael@gmail.com>
2023-04-23 17:10:11 +0200

    💄 adding constants to local storage

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export { RouteKeys } from './routeKeys'
 export { urlKeys } from './urlKeys'
+export { StorageKeys } from './storageKeys'
---
+++ src/constants/storageKeys.js
@@ -0,0 +1,4 @@
+export const StorageKeys = {
+  productsKey: 'products_storage',
+  productDetailKey: key => `products_details_${key}_storage`
+}
